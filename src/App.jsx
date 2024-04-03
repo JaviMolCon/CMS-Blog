f59ff515a35e18c151da9169056221a207b10d75
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-04-03 18:19:32 +0200

    React-router-dom implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes,Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import Post from '../src/pages/Post';
 
 function App() {
-  return <></>;
+  return (
+  <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home/:blog"/>
+      <Route path="/post" element={<Post />} />
+    </Routes>
+  </>
+  )
 }
-
 export default App;
