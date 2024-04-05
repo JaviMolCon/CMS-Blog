540b1dbb3e82fd4fe0aae4b16bdc51370030f867
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-04-05 13:45:25 +0200

    deleted unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import "./App.css";
 import { Routes,Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import Post from '../src/pages/Post';
+import { Post } from "./pages/Post";
+import { SingleEntry } from "./pages/SingleEntry";
 
 function App() {
   return (
   <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/home/:blog"/>
+      <Route path="/home/:blog" element={<SingleEntry />}/>
       <Route path="/post" element={<Post />} />
     </Routes>
   </>
