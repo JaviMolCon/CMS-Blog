7cd0b240c7f6c218f22943ba9063e4bdf8b96684
chadayananagha <159771211+chadayananagha@users.noreply.github.com>
2024-04-09 15:58:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Post } from "./pages/Post";
 import SingleEntry from "./pages/SingleEntry";
-import MainLayout from "./Layout/MainLayout";
+import MainLayout from "./layout/MainLayout";
 
 
 
