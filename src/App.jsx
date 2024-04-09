a13bc5094ce3ce117cfac784d5cf5910c79c477c
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-04-09 15:45:50 +0200

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Post } from "./pages/Post";
 import SingleEntry from "./pages/SingleEntry";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./Layout/MainLayout";
+
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={< MainLayout/>}>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/home/blogpost/:id" element={<SingleEntry />} />
