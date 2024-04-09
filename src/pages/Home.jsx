7633da67b8ae5be01c0a6a810433144a1e191b25
Javi Molcon <javier.molina.conesa@gmail.com>
2024-04-09 21:21:25 +0200

    NavBar and HeroSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Latest } from "../components/Latest";
+import HeroSection from "../components/HeroSection";
 
 export const Home = () => {
   return (
     <>
+      <HeroSection />
       <Latest />
     </>
   );
