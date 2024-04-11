333ca66314c2b8845b55f4d10bcb32601b07eac4
Juergen Adler <juergen.adler@gmx.net>
2024-04-11 15:37:18 +0200

    Posting works out now. Some GUI handling stuff left

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,53 +1,99 @@
+
+// Post.jsx
+
 import React from "react";
 import { useState } from "react";
+import { createClient } from "contentful-management";
+
 import Contact from "../components/Contact";
+
 
 export const Post = () => {
   const [formData, setFormData] = useState({
-    title: "",
-    author: "",
-    imageUrl: "",
-    comment: "",
+    title: '',
+    author: '',
+    imageUrl: '',
+    comment: '',
+    content: ''
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target;  
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: value
     });
   };
 
-  const handleSubmit = async (e) => {
-    window.alert("Your entry was not posted but let's pretend it was");
+  const handleSubmit = async (e) => { 
     e.preventDefault();
-    setFormData("");
 
     try {
-      // TODO: Find Contentful endpoint here....
-      const response = await fetch("/Guess-my-Endpoint", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      createBlogPost(formData);
+    }
+    catch (error) {
+      console.error("Error when invoking createBlogPost", error);
+    }
+  };
 
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
+  const createBlogPost = async (newBlogData) => {
+    try {
+      const strManagementAccessToken = import.meta.env.VITE_MANAGEMENT_ACCESS_TOKEN;
 
-      alert("Entry added successfully!");
+      // 1) Setup the client with our credentials. 
+      //
+      const client = createClient({ accessToken: strManagementAccessToken });
+      console.log(strManagementAccessToken);
+      const getSpace = await client.getSpace(import.meta.env.VITE_SPACE_ID);
+      const environment = await getSpace.getEnvironment('master');
+
+      // 2) Invoking the contentful content part. See_ Content part and content model in contentful
+      //
+      const newPost = await environment.createEntryWithId(
+        'blog',
+        crypto.randomUUID(),
+        {
+          fields: {
+            title: {
+              "en-US": newBlogData.title,
+            },
+            author: {
+              "en-US": newBlogData.author,
+            },
+            date: {
+              "en-US": new Date(Date.now()),
+            },
+            comments: {
+              "en-US": newBlogData.comment,
+            },
+            imgSrc: {
+              "en-US": newBlogData.imageUrl,
+            },
+            content: {
+              "en-US": newBlogData.content,
+            },
+          },
+        }
+      );
+
+      // 3) Posting.....
+      newPost.publish();
+
+      // 4) Reset the GUI
       setFormData({
         title: "",
         author: "",
         imageUrl: "",
         comment: "",
-      }); // Reset form after sucessful submit
-    } catch (error) {
-      console.error("Error:", error);
+        content: ""
+      });
+
+      alert("Entry added successfully!");
     }
-  };
+    catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <>
@@ -62,40 +108,54 @@ export const Post = () => {
         <div className="flex justify-center">
           <input
             type="text"
+            name="title"
+            value={FormData.title}
             placeholder="Enter a title"
             className="input input-bordered input-primary w-[650px]"
             required
             onChange={handleChange}
-            // value={formData.title}
           />
         </div>
         <div className="flex justify-center">
           <input
             type="text"
-            placeholder="Enter the name of author"
+            name="author"
+            value={FormData.author}
+            placeholder="Enter the name of the author"
             className="input input-bordered input-primary w-[650px]"
             required
             onChange={handleChange}
-            // value={formData.author}
           />
         </div>
         <div className="flex justify-center">
           <input
-            type="text"
+            type="url"
+            name="imageUrl"
+            value={FormData.imageUrl}
             placeholder="Enter the image url"
             className="input input-bordered input-primary w-[650px]"
             required
-            // value={formData.imageUrl}
             onChange={handleChange}
           />
         </div>
         <div className="flex justify-center">
           <textarea
-            className="textarea textarea-primary w-[650px] h-[500px]"
+            name="comment"
+            value={FormData.comment}
+            className="textarea textarea-primary w-[650px] h-[250px]"
             placeholder="Write content"
             required
             onChange={handleChange}
-            // value={formData.comment}
+          ></textarea>
+        </div>
+        <div className="flex justify-center">
+          <textarea
+            name="content"
+            value={FormData.content}
+            className="textarea textarea-primary w-[650px] h-[250px]"
+            placeholder="Write content"
+            required
+            onChange={handleChange}
           ></textarea>
         </div>
         <div className="flex justify-center w-full">
